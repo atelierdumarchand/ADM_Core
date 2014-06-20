2d1e1a18e98a05f7d6375de25bf47c45281d276f
Vincent Pietri <vincent.pietri@gmail.com>
2014-06-20 11:54:33 +0200

    Ajout de methode pour récupération des label des options d'attributs

--- app/code/community/ADM/Core/Helper/Data.php
+++ app/code/community/ADM/Core/Helper/Data.php
@@ -66,4 +66,45 @@ class ADM_Core_Helper_Data extends Mage_Core_Helper_Abstract
 
         return $options;
     }
+
+
+    /**
+     *
+     * @param string $attributeCode
+     * @param int $itemId
+     * @param int $storeId
+     * @param string $entityType
+     */
+    public function getAttributeOptionLabel($attributeCode, $itemId, $storeId=0, $entityType='catalog_product')
+    {
+        $cacheKey = 'ADM_CORE_ATTRIBUTE_' . $attributeCode . '_STORE_' . $storeId;
+        if (Mage::app()->useCache('config') && $cache = Mage::app()->loadCache($cacheKey)) {
+            $options = unserialize($cache);
+        } else {
+            $options=array();
+
+            $entityType = Mage::getModel('eav/config')->getEntityType($entityType);
+            $attributeModel = Mage::getModel('eav/entity_attribute')->loadByCode($entityType, $attributeCode);
+            $attributeCollection = Mage::getResourceModel('eav/entity_attribute_option_collection')
+            ->setAttributeFilter($attributeModel->getId())
+            ->setStoreFilter(0)
+            ->load();
+
+            foreach( $attributeCollection->toOptionArray() as $_cur_option ) {
+                $options[$_cur_option['value']]= $_cur_option['label'];
+            }
+
+
+            if (Mage::app()->useCache('config')) {
+                Mage::app()->saveCache(serialize($options), $cacheKey, array('config'));
+            }
+        }
+
+        if (isset($options[$itemId])) {
+            return $options[$itemId];
+        } else {
+            return '';
+        }
+    }
+
 }
